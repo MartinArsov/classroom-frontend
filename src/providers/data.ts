1d767b8e307eec931505d06e6d69e9b21e2a2e00
martinarsov78 <martinarsov78@gmail.com>
2026-06-10 20:44:56 +0200

    implement classes list page

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -42,6 +42,14 @@ const options: CreateDataProviderOptions = {
           if (field === 'department') params.department = value;
           if (field === 'name' || field === 'code') params.search = value;
         }
+
+        if (resource === 'classes') {
+          if (field === 'subject') params.subject = value;
+          if (field === 'status') params.status = value;
+          if (field === 'name' || field === 'inviteCode' || field === 'code') {
+            params.search = value;
+          }
+        }
       });
       return params;
     },
